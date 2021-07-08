b9d0b4332d6674aa118c70c7165e15e157278b95
rmxxtt <62797019+rmxxtt@users.noreply.github.com>
2021-07-09 02:18:16 +0700

    # TODO

--- src/editor.go
+++ src/editor.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"image/jpeg"
 	_ "image/png"
 	"io/ioutil"
+	"math/rand"
 	"os"
 )
 
@@ -44,6 +46,12 @@ func main() {
 		fmt.Print(err)
 		return
 	}
+
+	err = pass(&img)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
 }
 
 func ReadImageFile(path string, img *image.Image) error {
@@ -58,4 +66,33 @@ func ReadImageFile(path string, img *image.Image) error {
 	*img, _, err = image.Decode(file)
 
 	return fmt.Errorf("")
+}
+
+func pass(img *image.Image) error {
+	bounds := (*img).Bounds()
+	size := (*img).Bounds().Size()
+	rect := image.Rect(0, 0, size.X, size.Y)
+	newImg := image.NewRGBA(rect)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, a := (*img).At(x, y).RGBA()
+			r, g, b, a = r>>8, g>>8, b>>8, a>>8
+			newImg.Set(x, y, color.RGBA{
+				R: uint8(rand.Intn(255)),
+				G: uint8(rand.Intn(255)),
+				B: uint8(rand.Intn(255)),
+				A: uint8(rand.Intn(255)),
+			})
+		}
+	}
+
+	fmt.Print("Укажите путь сохранения изображения (без формата): ")
+	var path string
+	_, err := fmt.Scanln(&path)
+
+	file, err := os.Create(path + ".todo")
+	err = jpeg.Encode(file, newImg, nil)
+
+	return err
 }
